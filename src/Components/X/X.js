b8745c12e0e7f479aff47833d209d6b106c05e29
Christian Alston <xcda15@gmail.com>
2020-10-09 16:34:13 -0400

    close button added

---
+++ src/Components/X/X.js
@@ -0,0 +1,21 @@
+// import React, { useState } from 'react'
+// import NavMenu from '../NavMenu/NavMenu'
+
+// const X = () => {
+//     const [close, setClose] = useState(true)
+//     const styled = `
+//         position: absolute;
+//         font-size: 22px;
+//         top: 5%;
+//         left: 5%;
+//         color: rgb(0,214,203);
+//     `
+//     return (
+//         <div>
+//             <h1 onClick={() => setClose(!close)} style={styled}>X</h1>
+//             {close && <NavMenu />}
+//         </div>
+//     )
+// }
+
+// export default X
