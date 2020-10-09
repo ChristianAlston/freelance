758726189c8ca2c5582a64b133f894012deb970c
Christian Alston <xcda15@gmail.com>
2020-10-09 16:16:17 -0400

    useState changed to open nav

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 import NavMenu from '../NavMenu/NavMenu'
 import './Burger.css'
 
 const Burger = () => {
+    const [show, setShow] = useState(false)
+
     return (
         <div>
-            <NavMenu />
-
-            <div className='burger'>
+            <div className='burger' onClick={() => setShow(!show)} >
                 <div className='line1'></div>
                 <div className='line2'></div>
             </div>
+            {show && <NavMenu />}
         </div>
     )
 }
+
 
 export default Burger
